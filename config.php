5f9c93d3728ea1e5a2a5e5e8837ef1e676a8f23d
Gowthaman S <70950409+gowthaman99@users.noreply.github.com>
2020-10-29 10:21:14 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $config = array (
+  'db_host' => 'localhost',
+  'db_name' => 'timetable',
+  'db_user' => 'gowthaman',
+  'db_pswd' => 'oe0mfINYOGwCoRCv',
+  'ldap_host' => '',
+  'ldap_dn' => '',
+);
+?>
